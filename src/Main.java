5467066970244961db8168e1c6e12e02fbdfe84d
Your Name <you@example.com>
2023-12-21 00:04:26 +0600

    assignment

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        //Task1
+    Person person=new Person();
+    person.name="Akmagambetova Aliya";
+    person.age=17;
+        System.out.println("From task 1: Hello, my name is "+person.name+", I am "+person.age+" years old");
+        //Task 2
+        person.printDetails(person.name,person.age);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-            System.out.println("Aliya loh");
-            System.out.println("Anel Lox");
-        }
+        //Task 4
+        Person2 person2=new Person2("Yerkezhan",3);
+        person2.printDetails(person2.GetName(),person2.GetAge());
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,8 @@
+public class Person{
+    public String name;
+    public int age;
+    public void printDetails(String name,int age)
+    {
+        System.out.println("From task 2: Hello, my name is "+name+", I am "+age+" years old");
+    }
+}
